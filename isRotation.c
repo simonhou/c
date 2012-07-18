c38b5ba40a4e9f391c718ff10e6d3fafeb67d095
simon <houjun612@gmail.com>
2012-07-18 17:38:10 +0800

    V0.2
    Add KMP Algorithm

--- isRotation.c
+++ isRotation.c
@@ -21,6 +21,54 @@ int isSubstr(char* str1, char *str2)
 
 }
 
+int KMP(char* str1, char* str2)
+{
+    int next[100];   /* 用于保存判断前缀后缀长度 */
+    int i,j;
+    i = 0;
+    j = 0;
+    getnext(str2,next);
+    while( i < strlen(str1))
+    {
+        if( j = -1 || *(str1+i) == *(str2+j) )
+        {
+            i++;
+            j++;
+        }
+        else
+        {
+            j = next[j];  /* 与BF最大的不同是i不需要回溯了 */
+        }
+        if( j = strlen(str2))
+            return i - strlen(str2);
+    }
+
+    return -1;
+
+
+}
+
+void getnext(char* str,int *next)
+{
+    int k,j;
+    next[0] = -1;
+    j = 0;
+    k = -1;
+    while( j < strlen(str) - 1)
+    {
+        if( k = -1 || str[j] == str[k])
+        {
+            j++;
+            k++;
+            next[j] = k;
+        }
+        else
+         k = next[j];  /* Matching Failure */
+    }
+
+
+}
+
 int main()
 {
     char* str1 = "ababdeababc";
